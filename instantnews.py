a33ee822461a84cb02772140c1da2ec1a5af9e8e
Peter Maday <p_maday@apple.com>
2020-11-20 17:49:55 +0100

    Add text wrapping, update formatting

--- instantnews.py
+++ instantnews.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
-"""
-Author:Shivam Singh
-mail:shivam043@gmail.com
-copyright@2017
-"""
-
 
 import os
 import sys
 import argparse
-import webbrowser
 import requests
-
+import textwrap
 
 # Global constants
 API_URL = "https://newsapi.org/register"
@@ -19,11 +12,10 @@ BASE_URL = "https://newsapi.org/v1/articles"
 SOURCE_URL = "https://newsapi.org/v1/sources"
 VALID = ['y', 'n']
 NEWS_CATEGORIES = [
-    'business', 'entertainment', 'gaming',
-    'general', 'music', 'politics',
+    'business', 'entertainment', 'gaming', 'general', 'music', 'politics',
     'science-and-nature', 'sport', 'technology'
-    ]
-
+]
+WRAP_LINE_WIDTH = 100
 
 # Global variables
 news_codes = []
@@ -36,7 +28,9 @@ def test_network_connection():
         response = requests.get("https://newsapi.org/")
         response.raise_for_status()
     except requests.exceptions.ConnectionError:
-        print("There was issue connecting to the server. Please check your network connection.")
+        print(
+            "There was issue connecting to the server. Please check your network connection."
+        )
         sys.exit(1)
     except requests.exceptions.RequestException as exception:
         print(exception)
@@ -66,8 +60,7 @@ def load_config_key():
         print('No API Token detected. '
               'Please visit {0} and get an API Token, '
               'which will be used by instantnews '
-              'to get access to the data.'
-              .format(API_URL))
+              'to get access to the data.'.format(API_URL))
         sys.exit(1)
 
 
@@ -83,10 +76,11 @@ def show_sources_category(category):
         sys.exit(1)
 
     url = "?category={category_type}"
-    response = requests.get((SOURCE_URL+url).format(category_type=category))
+    response = requests.get((SOURCE_URL + url).format(category_type=category))
     json = response.json()
     for source in json['sources']:
-        print(u"{0}: <{1}> {2}".format("News Code", source['id'], source['name']))
+        print(u"{0}: <{1}> {2}".format("News Code", source['id'],
+                                       source['name']))
 
 
 def show_sources_all():
@@ -94,7 +88,8 @@ def show_sources_all():
     response = requests.get(SOURCE_URL)
     json = response.json()
     for source in json['sources']:
-        print(u"{0}: <{1}> {2}".format("News Code", source['id'], source['name']))
+        print(u"{0}: <{1}> {2}".format("News Code", source['id'],
+                                       source['name']))
 
 
 def show_categories():
@@ -106,39 +101,28 @@ def show_categories():
 def show_news(code, BASE_URL):
     """Display news with respect to news id"""
     url = "?source={news_id}&apiKey="
-    response = requests.get((BASE_URL+url+api_key).format(news_id=code))
+    response = requests.get((BASE_URL + url + api_key).format(news_id=code))
     json = response.json()
 
-    article_list = []
-    for article in json['articles']:
-        print('[{0}] {1}: {2}'.format(len(article_list), "Title", article['title']))
-        print('{0}: {1}'.format("Author", article['author']))
-        print('{0}: {1}'.format("Summary", article['description']))
+    for idx, article in enumerate(json['articles']):
+
+        title = article['title']
+        url = article['url']
+
+        if article['description']:
+            description = "\n".join(
+                textwrap.wrap(description, width=WRAP_LINE_WIDTH))
+        else:
+            description = "N/A"
+
+        print('')
         print('--------------------------------------------')
-        article_list.append(article['url'])
-
-    print("Would you like to open an article in a web browser? Enter (y/n): ")
-
-    choice = (input()).lower()
-    if choice not in VALID:
-        print("Invalid entry.")
-        sys.exit()
-
-    if choice == 'y':
-        while choice == 'y':
-            article_number = input("Enter an article number: ")
-            while not article_number.isdigit() or not 0 <= int(article_number) <= len(article_list):
-                print("Invalid article number. Try again.")
-                article_number = input("Enter an article number: ")
-
-            webbrowser.open(article_list[int(article_number)])
-
-            print("Would you like to view another article? Enter (y/n): ")
-            choice = (input()).lower()
-            if choice not in VALID:
-                sys.exit()
-            if choice == 'y':
-                continue
+        print('')
+        print(title)
+        print('')
+        print('{0}: {1}'.format("Summary", description))
+        print('')
+        print(f'URL: {url}')
 
 
 def parser():
@@ -150,13 +134,23 @@ def parser():
         print("Arguments needed. Use argument --help/-h for more information.")
     else:
         parser = argparse.ArgumentParser()
-        parser.add_argument("--show_all", "-sa", action="store_true",
+        parser.add_argument("--show_all",
+                            "-sa",
+                            action="store_true",
                             help="Shows all available news channel codes.")
-        parser.add_argument("--categories", "-c", action="store_true",
+        parser.add_argument("--categories",
+                            "-c",
+                            action="store_true",
                             help="Shows all available news categories.")
-        parser.add_argument("--show", "-s", action="store",
-                            help="Shows all news channel codes for a specified category.")
-        parser.add_argument("--news", "-n", type=str, help="Shows news articles "
+        parser.add_argument(
+            "--show",
+            "-s",
+            action="store",
+            help="Shows all news channel codes for a specified category.")
+        parser.add_argument("--news",
+                            "-n",
+                            type=str,
+                            help="Shows news articles "
                             "for a specified news channel code.")
         args = parser.parse_args()
 
